e7181ca800e39283a490ee378c3b66fc2608dbb9
tushar <swamitusharjee2018@gmail.com>
2024-01-04 00:28:34 +0530

    fetch hero by name and display successfully

--- index.js
+++ index.js
@@ -10,18 +10,17 @@ let hash = CryptoJS.MD5(st).toString();
 const submit = document.querySelector("#form");
 const input = document.querySelector("#search-input");
 const suggestion = document.querySelector("#displaySuggestion");
-
+const heroSuggestion = document.querySelector("#heroContainer");
 // function or events
 
-// fetch function
+// !fetch function
 async function apiCall(URL) {
   const response = await fetch(URL);
   const data = await response.json();
   return data.data.results;
 }
-// apiCall("t");
 
-// event on input tag
+// ! event on input tag
 input.addEventListener("input", async function () {
   let value = input.value.trim();
   console.log(value);
@@ -37,27 +36,68 @@ input.addEventListener("input", async function () {
   }
 });
 
-// suggestion box function 
-function displaySuggestions(dataes){
-    suggestion.innerHTML='';
-    
-    dataes.forEach(data => {
-        // console.log(data.name)
-        const suggestionElement = document.createElement('li');
-            // suggestionElement.className = 'suggestion';
-            suggestionElement.textContent = data.name;
+// ! suggestion box function
+function displaySuggestions(dataes) {
+  suggestion.innerHTML = "";
 
-            suggestionElement.addEventListener('click', function () {
-                input.value = data.name;
-                suggestion.style.display = 'none';
-            });
-            suggestion.appendChild(suggestionElement)
+  dataes.forEach((data) => {
+    // console.log(data.name)
+    const suggestionElement = document.createElement("li");
+    // suggestionElement.className = 'suggestion';
+    suggestionElement.textContent = data.name;
+
+    suggestionElement.addEventListener("click", function () {
+      input.value = data.name;
+      suggestion.style.display = "none";
     });
-
+    suggestion.appendChild(suggestionElement);
+  });
 }
 
 // event on form tag
-submit.addEventListener("submit", function (e) {
+submit.addEventListener("submit", async function (e) {
   e.preventDefault();
-  console.log(`hello`);
+  let value = input.value.trim();
+  suggestion.innerHTML = "";
+  if (value.length == 0) {
+    return;
+  }
+
+  const URL = `http://gateway.marvel.com/v1/public/characters?name=${value}&ts=${ts}&apikey=${public_key}&hash=${hash}`;
+  const data = await apiCall(URL);
+  heroDisplay(data);
 });
+function heroDisplay(dataes) {
+    dataes.forEach(data => {
+        console.log(data)
+        const heroContainer = document.createElement("div");
+    // heroElement.className = 'suggestion';
+    // basic details 
+    const heroName=document.createElement('h3');
+    heroName.textContent=data.name;
+    const heroImage=document.createElement('img');
+    const path=data.thumbnail.path+'.'+data.thumbnail.extension;
+    // console.log(path)
+    heroImage.src=path;
+    
+
+    const heroDetails=document.createElement('a');
+    heroDetails.textContent="More Details";
+
+
+
+
+    heroContainer.appendChild(heroName);
+    heroContainer.appendChild(heroImage);
+    heroContainer.appendChild(heroDetails);
+
+    // heroContainer.textContent = data.name;
+
+    heroContainer.addEventListener("click", function () {
+        
+    //   input.value = data.name;
+    //   suggestion.style.display = "none";
+    });
+    heroSuggestion.appendChild(heroContainer);
+    });
+}
