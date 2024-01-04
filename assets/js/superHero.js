ea5d937f3bef98f4a13f20d5e84f30c6d1985f05
tushar <swamitusharjee2018@gmail.com>
2024-01-04 15:57:30 +0530

    super hero detail page styling complete

--- assets/js/superHero.js
+++ assets/js/superHero.js
@@ -1,6 +1,6 @@
 const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get("id");
-console.log(id);
+// console.log(id);
 
 // api public key
 let public_key = "0ca107dcc83d2d017103b0096ad6d269";
@@ -21,31 +21,33 @@ async function apiCall() {
 
   const response = await fetch(URL);
   const data = await response.json();
-  console.log(data.data.results);
+  // console.log(data.data.results);
   let html = "";
   data.data.results.forEach((d) => {
-    console.log(d);
+    // console.log(d);
     const path = d.thumbnail.path + "." + d.thumbnail.extension;
-    console.log(path);
+    // console.log(path);
 
     html = `
-    <div>
+    <div class="profile-div">
     <div>
     <img src=${path} alt ="Hero Image"/></div>
-    <div>
+    <div class="profile-details">
     <h1>${d.name} </h1>
     <p>${d.description}</p>
     <h3>Available Comics : ${d.comics.available} </h3>
-    <h3>Available Series : ${d.series.available} </h3>
-    <h3>Available Stories : ${d.stories.available} </h3>
-    
-    </div>
+    <h3>Total Series : ${d.series.available} </h3>
+    <h3>Total Stories : ${d.stories.available} </h3>
+    <div class="link" >
     <h1>Read More About ${d.name} </h1>
         <h3><a href=${d.urls[0].url} target="_blank">Marval </a></h3>
         
         <h3><a href=${d.urls[1].url} target="_blank">Wiki</a></h3>
-        <h3><a href=${d.urls[2].url} target="_blank"> Comic Link </a></h3>
+        <h3><a href=${d.urls[2].url} target="_blank"> Comic Link </a></h3></div>
     </div>
+    
+    </div>
+    
 
     `;
   });
